940f3f70ba9cbce01101d912ce8472f461759ca5
leomet07 <leomet07@gmail.com>
2024-07-08 16:36:34 -0400

    Fetch the lakes from CSV

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+
+	onMount(async () => {
+		console.log('Number of lakes: ', data.lakes.length);
+	});
+</script>
+
+<h1>Welcome to HAB Oracle</h1>
