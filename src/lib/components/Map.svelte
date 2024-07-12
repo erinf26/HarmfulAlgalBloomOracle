9d8d12edcf6b0438854f05ff3783377683da8bad
leomet07 <leomet07@gmail.com>
2024-07-12 16:30:33 -0400

    Create overlay function

--- src/lib/components/Map.svelte
+++ src/lib/components/Map.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import type { Map } from 'leaflet';
+	import type { LatLngBounds, Map } from 'leaflet';
 	import { browser } from '$app/environment';
 	import type { LakeExported } from '$lib/types';
 	import 'leaflet/dist/leaflet.css';
@@ -14,9 +14,23 @@
 		if (browser) {
 			const leaflet = await import('leaflet');
 
+			const add_lake_overlay_to_map = function (
+				imageUrl: string,
+				latLngBounds: LatLngBounds,
+				altText: string
+			) {
+				let imageOverlay = leaflet
+					.imageOverlay(imageUrl, latLngBounds, {
+						opacity: 0.8,
+						alt: altText,
+						interactive: true
+					})
+					.addTo(map);
+			};
+
 			map = leaflet
 				.map(mapElement, { preferCanvas: true }) // use canvas for better performance
-				.setView([42.18778778, -79.42924043], 11); // this sets the view for lake chautauqua
+				.setView([41.13612064886357, -73.9352407747292], 11); // this sets the view for lake chautauqua
 			// .setView([lakes[7].latitude, lakes[7].longitude], 7); // this sets the view for new york state
 
 			leaflet
@@ -42,20 +56,23 @@
 					});
 			}
 
-			let imageUrl = '/S2_Chautauqua_v2_predictions.png';
-			let altText = 'Lake Chautauqua';
-			let latLngBounds = leaflet.latLngBounds([
-				[42.26008723694799, -79.49540093993534], // Lake Chautauqua corner bounds
-				[42.0983120459646, -79.26995135973668]
-			]);
+			add_lake_overlay_to_map(
+				'/Congers_T2_predictions.png',
+				leaflet.latLngBounds([
+					[41.14790209037696, -73.94413139827235],
+					[41.13612064886357, -73.9352407747292]
+				]),
+				'Lake Congers'
+			);
 
-			let imageOverlay = leaflet
-				.imageOverlay(imageUrl, latLngBounds, {
-					opacity: 0.8,
-					alt: altText,
-					interactive: true
-				})
-				.addTo(map);
+			add_lake_overlay_to_map(
+				'/S2_Chautauqua_v2_predictions.png',
+				leaflet.latLngBounds([
+					[42.26008723694799, -79.49540093993534], // Lake Chautauqua corner bounds
+					[42.0983120459646, -79.26995135973668]
+				]),
+				'Lake Chautauqua'
+			);
 		}
 	});
 </script>
