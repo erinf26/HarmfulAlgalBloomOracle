bb9b712ee45d071b66ca0fdbfb9df6ffcb907fc8
leomet07 <leomet07@gmail.com>
2024-07-16 12:10:19 -0400

    Remove unused styles

--- src/routes/algal-blooms/+page.svelte
+++ src/routes/algal-blooms/+page.svelte
@@ -13,40 +13,7 @@
 </script>
 
 <h1>Algal Blooms Map</h1>
+
 <Searchbar lakes={data.lakes} />
+
 <Map lakes={data.lakes} />
-<!-- 
-<style>
-	.container {
-		position: relative;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-	}
-
-	.search-container {
-		position: absolute;
-		top: 1rem;
-		right: 1rem;
-		width: 200px; /* Adjust the width as needed */
-	}
-
-	.search-results {
-		position: absolute;
-		top: 70px;
-		right: 10px;
-		background: white;
-		border: 1px solid #ccc;
-		max-height: 300px;
-		overflow-y: auto;
-	}
-
-	.search-result-item {
-		padding: 0.5rem;
-		cursor: pointer;
-	}
-
-	.search-result-item:hover {
-		background: #f0f0f0;
-	}
-</style> -->
